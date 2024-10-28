0482538b77818cef8f8dacd623aa5c0a3e2e5ecc
AlejandroRibera1987 <alejandroribera1987@gmail.com>
2024-10-27 21:05:55 -0300

    Pagina productos terminada

--- pages/productos.php
+++ pages/productos.php
@@ -1,6 +1,16 @@
 <?php
 
 include_once('../components/header.php');
+include_once('../components/config/conection.php');
+
+    if($conection != NULL){
+
+        $consulta = "SELECT p.id_producto, p.nombre_producto, p.descripcion_producto, c.nombre_categoria, p.pais, p.precio, p.stock, p.img_producto, p.fk_categoria
+        FROM productos p
+        INNER JOIN categorias c ON p.fk_categoria = c.id_categoria";
+        $resultado = mysqli_query($conection, $consulta);
+
+    }
 
 ?>
 
@@ -25,26 +35,32 @@ include_once('../components/header.php');
                 </div>
             </aside>
             <section class="productos">
-                <div class="productos_lista">
-                    <div class="cards_producto_lista">
-                        <div class="titulo_img">
-                            <h2>Nombre de la moneda</h2>
-                            <figure>
-                                <img src="../img/moneda1.jpg" alt="Foto de la moneda">
-                            </figure>
-                        </div>
-                        <div class="descripcion_cards_producto">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem .</p>
-                            <div class="categoria_precio_cards">
-                                <p>Oro</p>
-                                <p>$17367</p>
+                <?php 
+                    while($fila = mysqli_fetch_array($resultado)){
+                        echo "
+                            <div class='productos_lista'>
+                                <div class='cards_producto_lista'>
+                                    <div class='titulo_img'>
+                                        <h2>$fila[nombre_producto]</h2>
+                                        <figure>
+                                            <img src='../img_db/$fila[img_producto]' alt='Foto de la moneda'>
+                                        </figure>
+                                    </div>
+                                    <div class='descripcion_cards_producto'>
+                                        <p>$fila[descripcion_producto]</p>
+                                        <div class='categoria_precio_cards'>
+                                            <p>$fila[nombre_categoria]</p>
+                                            <p>$$fila[precio]</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class='btn_cards_producto'>
+                                    <a href='productoindividual.php?id=$fila[id_producto]'>Ver más</a>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="btn_cards_producto">
-                        <a href="">Ver más</a>
-                    </div>
-                </div>
+                        ";
+                    }
+                ?>
             </section>
         </div>
     </main>
