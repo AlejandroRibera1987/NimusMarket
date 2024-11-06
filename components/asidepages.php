3e3dd5a573aa3be46fca5add230256e13f8c6246
AlejandroRibera1987 <alejandroribera1987@gmail.com>
2024-11-06 20:24:07 -0300

    Actualizado de Aside

--- components/asidepages.php
+++ components/asidepages.php
@@ -1,11 +1,22 @@
-            
+<?php
+    include_once("config/conection.php");
+
+    $consulta_categorias = "SELECT * FROM `categorias`";
+    $resultado_categorias = mysqli_query($conection, $consulta_categorias);
+
+?>
             <aside class="aside_categoria">
                 <h2>Categorias</h2>
                 <div class="lista">
                     <ul>
-                        <li><a href="../pages/productoscategoria.php?id=1">Monedas de Oro</a></li>
-                        <li><a href="../pages/productoscategoria.php?id=2">Monedas de Plata</a></li>
-                        <li><a href="../pages/productoscategoria.php?id=3">Monedas de Bronce</a></li>
+
+                    <?php
+                        while($fila = mysqli_fetch_array($resultado_categorias)){
+                            echo "<li><a href='../pages/productoscategoria.php?id=$fila[id_categoria]'>$fila[nombre_categoria]</a></li>";
+                        }
+
+                    ?>
+
                     </ul>
                 </div>
                 <h2>Ofertas</h2>
