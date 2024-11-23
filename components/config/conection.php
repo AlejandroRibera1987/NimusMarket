697eec8391b97f19ec483259b4b82fb160ce3099
AlejandroRibera1987 <alejandroribera1987@gmail.com>
2024-11-23 15:10:16 -0300

    Modificaciones para cargar a Servidor

--- components/config/conection.php
+++ components/config/conection.php
@@ -1,10 +1,10 @@
 <?php
 
 
-define('servidor', 'localhost');
-define('usuario', 'root');
-define('clave', '');
-define('base_de_datos', 'bdcoins');
+define('servidor', 'sql108.infinityfree.com');
+define('usuario', 'if0_37607652');
+define('clave', 'D45ADMIN2024');
+define('base_de_datos', 'if0_37607652_bd_nimusmarket');
 define('puerto', '3306');
 
 $conection = mysqli_connect(servidor, usuario, clave, base_de_datos, puerto);
